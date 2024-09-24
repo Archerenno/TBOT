31906a38fde7d5562302eb1f3bc5a267aac691e3
Archie Simpson <archer.simpson@icloud.com>
2024-09-24 15:17:08 +1200

    Add files via upload

---
+++ Crypto Trading Bot/multi_coin_testing.py
@@ -0,0 +1,73 @@
+"""
+Archer Simpson
+24/9/24
+Trading Bot Project - Using a Binance Testnet
+"""
+
+from tradingview_ta import TA_Handler, Interval, Exchange
+import tradingview_ta
+from binance.client import Client
+from binance.enums import *
+import binance
+import time
+
+# Testnet API credentials
+# ARCHERS'S KEYS
+API_KEY = 'QYHKtmBofXUNuHBJ352DG2jSAm9nz512wtDzteeKHvvGuFCXnJgw92xCbBiHJHfb'
+API_SECRET = 'hSjOPnrSNzwZW592nhil2sBFpvEK24szznBOGIULGClSFfoNmDoOjfUNAYO2NPES'
+
+# Base URL for Binance Testnet
+testnet_url = 'https://testnet.binance.vision/api'
+
+# Create a client instance for the testnet
+client = Client(API_KEY, API_SECRET, testnet=True)
+client.API_URL = testnet_url
+
+
+def print_all_available_coins():
+    """Prints the tickers of all available coins through the Binance Exchange."""
+    info = client.get_account()
+    all_balances = info['balances']
+    print("This is a list of all the available coins through Binance")
+    for coin in all_balances:
+        print(coin['asset'])
+
+
+def print_testnet_account_balance(symbol):
+    """Prints the testnet account balance (this is different from the balance printed in the run bot loop)"""
+    info = client.get_account()
+    all_balances = info['balances']
+    # This for loop searches all of the balances for every symbol until it finds the one specified
+    for coin in all_balances:
+        if coin['asset'] == symbol:
+            # The 'free' key in the dictionary says how much of the coin/currency you have available to use/spend
+            account_balance = coin['free']
+    print(f"Official Testnet Account balance ({symbol}): {float(account_balance):.4f}")
+
+
+def print_coin_information(symbol):
+    """NOTE: Only works for LOT_SIZE"""
+    # The indexing of [1] at the end of the statement below is section that specifies the filertype LOT_SIZE.
+    # Change this indexing if you want other filter types
+    coin_info = client.get_symbol_info(symbol)['filters'][1]
+    print(f"Coin: {symbol}")
+    print(f"Type: {coin_info['filterType']}")
+    print(f"    - Minimum Coin you can hold: {coin_info['minQty']}")
+    print(f"    - Max Coin you can hold: {coin_info['maxQty']}")
+    print(f"    - Step Size (Minimum order amount): {coin_info['stepSize']}")
+
+
+def print_all_coins_info():
+    coins = client.get_ticker()
+    usdt_coins = [(coin['symbol'], coin['priceChangePercent'], coin['volume']) for coin in coins if coin['symbol'].endswith('USDT')]
+    price_change_percent = lambda x: x[1]
+    sorted_usdt_coin = sorted(usdt_coins, key=price_change_percent)
+    print(sorted_usdt_coin)
+
+
+
+def main():
+    print_all_coins_info()
+
+
+main()
